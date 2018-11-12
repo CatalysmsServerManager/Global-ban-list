1b8f809f67e3f7450cebfe50bbcc08d8ee569b6b
Niek Candaele <niekcandaele@gmail.com>
2018-11-12 14:02:42 +0100

    Add custom error modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-console.log('Initializing app');
+function showErrorModal(errorMessage) {
+  const errorModalElem = $('#modal-error');
+  // Adding an event listener every time the error modal is shows is not 100% efficient
+  // If a user doesn't cause a lot of errors, he gets rewarded with better page performance <.<
+  errorModalElem.on('show.bs.modal', () => {
+    errorModalElem.find('.modal-error-message').text(`Something went wrong. ${errorMessage}`);
+  });
+
+  errorModalElem.modal('show');
+}
+
+$(document).ready(() => {
+  console.log('Initializing app');
+  showErrorModal('Testing');
+});
