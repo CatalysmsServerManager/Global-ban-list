a713c1b5e97a3a0432823e3e99a32e9b93171622
Niek Candaele <niekcandaele@gmail.com>
2018-11-13 15:13:21 +0100

    Add seeder for supported games

--- seeders/20181113113958-default-games.js
+++ seeders/20181113113958-default-games.js
@@ -2,10 +2,20 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Games', [{
-      id: 1,
-      name: '7 Days to Die',
-    }
-  ], {}),
+    id: 1,
+    fullName: '7 Days to Die',
+    code: '7d2d',
+  },
+  {
+    id: 2,
+    fullName: 'Rust',
+    code: 'rust',
+  },
+  {
+    id: 3,
+    fullName: 'Minecraft',
+    code: 'mc',
+  }], {}),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Games', null, {})
 };
