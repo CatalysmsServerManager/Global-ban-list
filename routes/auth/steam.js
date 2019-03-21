6725df4bc310d3d61a9e039e5c256275d8d1afd9
Niek Candaele <niekcandaele@gmail.com>
2019-03-21 22:19:42 +0100

    Add documentation for steam auth

--- routes/auth/steam.js
+++ routes/auth/steam.js
@@ -1,5 +1,10 @@
 const passport = require('passport');
-
+/**
+ * @api {GET} /auth/steam GET /auth/steam
+ * @apiName Steam authentication
+ * @apiGroup Authentication
+ * @apiDescription Will redirect to steam OpenID authentication
+ */
 module.exports = function steamAuth(app) {
   app.get('/auth/steam', passport.authenticate('steam'), () => {
 
--- routes/auth/steamReturn.js
+++ routes/auth/steamReturn.js
@@ -1,6 +1,11 @@
 const passport = require('passport');
-
-module.exports = function steamAuth(app) {
+/**
+ * @api {GET} /auth/steam/return GET /auth/steam/return
+ * @apiName Steam authentication return
+ * @apiGroup Authentication
+ * @apiDescription Return after authentication via steam
+ */
+module.exports = function steamAuthReturn(app) {
   app.get('/auth/steam/return',
     passport.authenticate('steam', {
       failureRedirect: '/login',
