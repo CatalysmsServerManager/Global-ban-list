50e4e8b5e0752d4e34c0b5d08f18567d73ab7822
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-21 19:13:27 +0000

    Deploy CatalysmsServerManager/Global-ban-list to github.com/CatalysmsServerManager/Global-ban-list.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Global ban list",
+  "version": "0.0.1",
+  "description": "API documentation for the global ban list",
+  "title": "GBL API",
+  "url": "https://api.github.com/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-21T19:12:54.687Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
